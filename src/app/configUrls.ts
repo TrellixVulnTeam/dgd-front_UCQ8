d1308be20c1b4fc29216b0cb8b54c9f9f1068c73
saeed <saeedrasooli421@gamil.com>
2021-07-07 21:18:44 +0430

    activity users

--- src/app/configUrls.ts
+++ src/app/configUrls.ts
@@ -1,4 +1,4 @@
-export const  BaseUrl = 'http://0.0.0.0:8000/api/';
+export const  BaseUrl = 'http://localhost:8000/api/';
 // export const  BaseUrl = 'http://127.0.0.1:8000/api/';
 
 export const GetProvincesUrl =  BaseUrl + 'provinces'
